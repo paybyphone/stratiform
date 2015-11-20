2693037d3c84449b838201cbed4d02c7933e0da0
Chris Marchesi <cmarchesi@paybyphone.com>
2015-11-19 20:56:58 -0800

    De-reeking Stratiform::Stack and moving cloudformation connection direct to class

--- lib/stratiform/stack.rb
+++ lib/stratiform/stack.rb
@@ -7,42 +7,36 @@ module Stratiform
 
     def initialize(name, &block)
       @name = name
-      instance_eval(&block) unless block.nil?
+      instance_eval(&block) if block
     end
 
-    def name(arg = nil)
-      @name = arg unless arg.nil?
+    def name(name_value = nil)
+      @name = name_value if name_value
       @name
     end
 
-    def targets(arg = nil)
-      @targets = arg unless arg.nil?
+    def targets(targets_value = nil)
+      @targets = targets_value if targets_value
       @targets
     end
 
-    def template_data(arg = nil)
-      @template_data = arg unless arg.nil?
+    def template_data(template_data_value = nil)
+      @template_data = template_data_value if template_data_value
       @template_data
     end
 
-    def parameters(arg = nil)
-      @parameters = arg unless arg.nil?
+    def parameters(parameters_value = nil)
+      @parameters = parameters_value if parameters_value
       @parameters
     end
 
-    def validate_template(target)
-      cloudformation = cloudformation_client(
-        target.region,
-        target.target_credentials
-      )
+    def validate_template
+      cloudformation = Aws::CloudFormation::Client.new
       cloudformation.validate_template(template_body: read_template)
     end
 
-    def create_stack(target)
-      cloudformation = cloudformation_client(
-        target.region,
-        target.target_credentials
-      )
+    def create_stack
+      cloudformation = Aws::CloudFormation::Client.new
       resp = cloudformation.create_stack(
         stack_name: @name,
         template_body: read_template
