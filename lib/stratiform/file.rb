34ff57fad470c0ec6ec9e48e8169f0475d358765
Chris Marchesi <cmarchesi@paybyphone.com>
2015-11-19 21:22:26 -0800

    Removing file.rb until it's implemented

--- lib/stratiform/file.rb
+++
@@ -1,9 +0,0 @@
-module Stratiform
-  # Common utilities for file content.
-  module File
-    # Read in the contents of a file.
-    def file_content(filename)
-      IO.read(filename)
-    end
-  end
-end
