7a242239fd5eab5a027ad85572ff24973e2e5189
Chris Marchesi <cmarchesi@paybyphone.com>
2015-11-20 16:05:00 -0800

    Refactoring of DSL, more to come

--- lib/stratiform/dsl.rb
+++ lib/stratiform/dsl.rb
@@ -6,69 +6,46 @@ module Stratiform
   class DSL
     attr_reader :targets, :stacks, :run_data
 
+    # Reads the Stratifile data in for loading. Defaults to "Stratifile"
+    # in the current working directory for now.
+    def self.read_stratifile
+      IO.read('Stratifile')
+    end
+
     def initialize
-      @targets = []
-      @stacks = []
-      @run_data = {}
+      @targets = {}
+      @stacks = {}
     end
 
     # Wraps DSL for Stratiform::Target and loads in targets.
-    def target(name, &block)
-      if @targets.index { |x| true if x.name == name }
-        fail NameError, "target with name #{name} already exists"
+    def target(target_name, &block)
+      if @targets.include?(target_name)
+        fail NameError, "target with name #{target_name} already exists"
       end
-      @targets.push(Stratiform::Target.new(name, &block))
+      @targets[target_name] = Stratiform::Target.new(target_name, &block)
     end
 
     # Wraps DSL for Stratiform::Stack and loads in stacks.
-    def stack(name, &block)
-      if @stacks.index { |x| true if x.name == name }
-        fail NameError, "stack with name #{name} already exists"
+    def stack(stack_name, &block)
+      if @stacks.include?(stack_name)
+        fail NameError, "stack with name #{stack_name} already exists"
       end
-      @stacks.push(Stratiform::Stack.new(name, &block))
+      @stacks[stack_name] = Stratiform::Stack.new(stack_name, &block)
     end
 
     def run_create(target_name, stack_name)
-      target = target_byname(target_name)
-      stack = stack_byname(stack_name)
-      task_rundata_initialize(target_name, stack_name)
+      stack = @stacks[stack_name]
       unless stack.targets.include?(target_name)
         fail(
           IndexError,
           "target #{target_name} is not a member of stack #{stack_name}"
         )
       end
-      task_rundata = @run_data[target_name][stack_name]
-      task_rundata['stack_id'] = stack.create_stack
+      stack.create_stack
     end
 
     def load_stratifile
-      instance_eval(read_stratifile, 'Stratifile')
-    end
-
-    def report_actions
-    end
-
-    # Reads the Stratifile data in for loading. Defaults to "Stratifile"
-    # in the current working directory for now.
-    def read_stratifile
-      IO.read('Stratifile')
-    end
-
-    # initializes the run data for a task
-    def task_rundata_initialize(target_name, stack_name)
-      @run_data[target_name] ||= {}
-      @run_data[target_name][stack_name] ||= {}
-    end
-
-    def target_byname(name)
-      index = @targets.index { |x| true if x.name == name }
-      @targets[index]
-    end
-
-    def stack_byname(name)
-      index = @stacks.index { |x| true if x.name == name }
-      @stacks[index]
+      instance_eval(self.class.read_stratifile, 'Stratifile')
     end
   end
 end
--- spec/stratiform/dsl_spec.rb
+++ spec/stratiform/dsl_spec.rb
@@ -11,54 +11,31 @@ describe Stratiform::DSL do
     it 'has an empty stack list' do
       expect(dsl.stacks).to be_empty
     end
-    it 'has empty rundata' do
-      expect(dsl.run_data).to be_empty
-    end
   end
 
   describe '#load_stratifile' do
     it 'loads a target of the right class' do
       dsl = Stratiform::DSL.new
       dsl_mock_and_load(dsl)
-      expect(dsl.targets[0].class.name).to eq('Stratiform::Target')
+      expect(dsl.targets['test-target'].class.name).to eq('Stratiform::Target')
     end
 
     it 'sets the name of the target' do
       dsl = Stratiform::DSL.new
       dsl_mock_and_load(dsl)
-      expect(dsl.targets[0].name).to eq('test-target')
+      expect(dsl.targets['test-target'].name).to eq('test-target')
     end
 
     it 'loads a stack of the right class' do
       dsl = Stratiform::DSL.new
       dsl_mock_and_load(dsl)
-      expect(dsl.stacks[0].class.name).to eq('Stratiform::Stack')
+      expect(dsl.stacks['test-stack'].class.name).to eq('Stratiform::Stack')
     end
-  end
 
-  describe '#target_byname' do
-    it 'loads a target by name' do
+    it 'sets the name of the stack' do
       dsl = Stratiform::DSL.new
       dsl_mock_and_load(dsl)
-      target = dsl.target_byname('test-target')
-      expect(target.class.name).to eq('Stratiform::Target')
-    end
-  end
-
-  describe '#stack_byname' do
-    it 'loads a stack by name' do
-      dsl = Stratiform::DSL.new
-      dsl_mock_and_load(dsl)
-      stack = dsl.stack_byname('test-stack')
-      expect(stack.class.name).to eq('Stratiform::Stack')
-    end
-  end
-
-  describe '#task_rundata_initialize' do
-    it 'creates null run data for a task' do
-      dsl = Stratiform::DSL.new
-      dsl.task_rundata_initialize('test-target', 'test-stack')
-      expect(dsl.run_data['test-target']['test-stack']).to be_empty
+      expect(dsl.stacks['test-stack'].name).to eq('test-stack')
     end
   end
 
@@ -67,7 +44,7 @@ describe Stratiform::DSL do
       dsl = Stratiform::DSL.new
       dsl_mock_and_load(dsl)
       dsl.run_create('test-target', 'test-stack')
-      stack_id = dsl.run_data['test-target']['test-stack']['stack_id']
+      stack_id = dsl.run_create('test-target', 'test-stack')
       expect(stack_id).to eq('StackId')
     end
   end
