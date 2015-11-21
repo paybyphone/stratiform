bc8f60d9e7d5d70bbcfe09ffa824a6471d615fba
Chris Marchesi <cmarchesi@paybyphone.com>
2015-11-21 15:18:19 -0800

    Refactoring AwsConfig as a module with refactored testing as well

--- lib/stratiform/awsconfig.rb
+++ lib/stratiform/awsconfig.rb
@@ -4,11 +4,11 @@ require 'securerandom'
 
 module Stratiform
   # AWS related configuration information and authentication manager
-  class AwsConfig
-    attr_reader :aws_default_region, :aws_default_credentials
+  module AwsConfig
+    module_function
 
     # Loads the config section for a specific profile.
-    def self.load_config_value(profile, key)
+    def load_config_value(profile:, key:)
       profile = "profile #{profile}" unless profile == 'default'
       config_file = File.expand_path('~/.aws/config')
       aws_config = IniFile.load(config_file) if File.exist?(config_file)
@@ -17,8 +17,8 @@ module Stratiform
     end
 
     # Checks to see if MFA is required for a specific profile.
-    def self.mfa_required(profile)
-      if load_config_value(profile, 'mfa_serial')
+    def mfa_required(profile:)
+      if load_config_value(profile: profile, key: 'mfa_serial')
         true
       else
         false
@@ -26,7 +26,7 @@ module Stratiform
     end
 
     # Assume role and set credentials to that role.
-    def self.assume_aws_role(role_arn)
+    def assume_aws_role(role_arn:)
       session_name = "#{role_arn.split('/')[-1]}_#{SecureRandom.hex(8)}"
       Aws.config.update(
         credentials: Aws::AssumeRoleCredentials.new(
@@ -37,17 +37,18 @@ module Stratiform
     end
 
     # Creates an AWS session and returns the credentails object.
-    def self.create_aws_session(profile, mfa_code)
+    def create_aws_session(profile:, mfa_code:)
       sts = Aws::STS::Client.new
       sts.get_session_token(
-        serial_number: load_config_value(profile, 'mfa_serial'),
+        serial_number: load_config_value(profile: profile, key: 'mfa_serial'),
         token_code: mfa_code
       ).credentials
     end
 
+    # :reek:FeatureEnvy
     # Creates the base credentials
-    def self.default_credentials(profile, mfa_code)
-      session = create_aws_session(profile, mfa_code)
+    def default_credentials(profile:, mfa_code:)
+      session = create_aws_session(profile: profile, mfa_code: mfa_code)
       Aws.config.update(
         access_key_id: session.access_key_id,
         secret_access_key: session.secret_access_key,
@@ -57,39 +58,34 @@ module Stratiform
     end
 
     # resets the default credentials in Aws.config to the default credentials.
-    def self.reset_credentials
+    def reset_credentials
       Aws.config.delete(:credentials)
     end
 
-    # Sets defaults from config and command-line options, and configures
-    # AWS session, if MFA has been supplied.
-    def initialize
-      @aws_default_region = nil
-      @aws_default_credentials = nil
+    # Sets the region.
+    def update_region(region:)
+      Aws.config.update(region: region)
+    end
+
+    # Deletes the region.
+    def delete_region
+      Aws.config.delete(:region)
     end
 
     # Sets the default region from:
     #  - Supplied region (from options)
+    #  - ENV['AWS_REGION'] if set
     #  - Region located in configuration
     #
     # If region is not set here, we do not set it, and region will rely on
     # either ENV['AWS_REGION'] (SDK behaviour) or static region set in target
     # config.
-    def default_region(option_region, option_profile)
-      config_region = self.class.load_config_value(option_profile, 'region')
-      @aws_default_region = config_region if config_region
-      @aws_default_region = option_region if option_region
-      reset_region
-    end
-
-    # :reek:DuplicateMethodCall
-    # resets the default region in Aws.config to the default region.
-    def reset_region
-      if @aws_default_region
-        Aws.config.update(region: @aws_default_region)
-      else
-        Aws.config.delete(:region)
-      end
+    def default_region(option_region: nil, profile: 'default')
+      config_region = load_config_value(profile: profile, key: 'region')
+      env_region = ENV['AWS_REGION']
+      return option_region if option_region
+      return env_region if env_region
+      return config_region if config_region
     end
   end
 end
--- spec/stratiform/helpers/awsconfig_spec.rb
+++ spec/stratiform/helpers/awsconfig_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 def mock_aws_config
+  Aws.config = { stub_responses: true }
   real_cfg = '~/.aws/config'
   mock_cfg = File.expand_path('../test_files/aws_config', __FILE__)
+  allow(File).to receive(:expand_path).and_call_original
   allow(File).to receive(:expand_path).with(real_cfg).and_return(mock_cfg)
 end
